4c0b7578d3596362109b7d0c03cef81664b01a1f
Lee Leathers <leeleathers@gmail.com>
2015-08-21 18:11:17 -0700

    init

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function(grunt) {
+
+  // Initialize Grunt
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    watch: {
+      options: {
+        livereload: true
+      },
+      sass: {
+        files: '<%= files.sass %>',
+        tasks: ['compass']
+      }
+    },
+    compass: {
+      dist: {
+        options: {
+          sassDir: 'sass',
+          cssDir: 'dist/css'
+        }
+      }
+    }
+  });
+
+  /* Load Grunt plugins */
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+
+  grunt.registerTask('default', ['compass:dist']);
+};
---
+++ README.md
@@ -0,0 +1,17 @@
+# CitizenNet's UI Kit #
+
+## Build The Project ##
+* grunt
+
+## Running Web App ##
+* php -S localhost:8000
+* open http://localhost:8000/examples in your browser
+
+## Dev Dependencies ##
+* brew install npm
+* sudo gem install compass
+* sudo npm install
+
+## Developing ##
+* Developing can be done on localhost. Your user will be auto-logged in.
+* Just run "grunt watch" to concat your scss files.
