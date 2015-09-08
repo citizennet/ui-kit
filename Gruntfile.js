761b7173392c5beda2756f5ae3512c355344fad5
Lee Leathers <leeleathers@gmail.com>
2015-09-08 15:52:29 -0700

    style(*): include fonts

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,7 +8,7 @@ module.exports = function(grunt) {
         livereload: true
       },
       sass: {
-        files: 'sass/*',
+        files: 'sass/common.scss',
         tasks: ['compass:dist']
       }
     },
