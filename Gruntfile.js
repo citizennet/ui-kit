39633fb72870767edb4df06f62f0126e3dabfb4d
Lee Leathers <leeleathers@gmail.com>
2015-09-29 12:04:23 -0700

    chore(Gruntfile.js): turn off livereload

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,7 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     watch: {
       options: {
-        livereload: true
+        //livereload: true
       },
       sass: {
         files: [
